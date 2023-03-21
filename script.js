369c9c35f7199eab115bccf64cbef267cf05b252
Intikhabislam <124589066+Intikhabislam@users.noreply.github.com>
2023-03-21 10:45:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Your Script here.
 
 const lookup = {
@@ -13,7 +14,12 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
+	const string = encodedStr;
+	const arr = [];
+ for(let i = 0;i<string.length;i++){
+	string.charAt(i) = string.charAt(i+13);
+	 arr.push(string[i]);
+ }
   return ;//return decodedArr
 }
 
